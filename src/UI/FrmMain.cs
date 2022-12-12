3f0db5c0f706225370f753d645496a3ebbd1cb3a
AaronF11 <aaron.flores7908@alumnos.udg.mx>
2022-12-11 23:59:48 -0600

    4. Renombramiento de CSpeech.

--- src/UI/FrmMain.cs
+++ src/UI/FrmMain.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Speech.src.Class;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,8 +11,22 @@ using System.Windows.Forms;
 
 namespace Speech
 {
+    //-------------------------------------------------------------------------
+    // Class: FrmMain
+    // Description: This class is used to create the main form
+    // Date: 11/12/2022
+    //-------------------------------------------------------------------------
     public partial class FrmMain : Form
     {
+        //-------------------------------------------------------------------------
+        // Attributes
+        //-------------------------------------------------------------------------
+        CSpeech Speech;
+        //-------------------------------------------------------------------------
+        // Constructor: FrmMain
+        // Description: This constructor is used to create the main form
+        // Date: 11/12/2022
+        //-------------------------------------------------------------------------
         public FrmMain()
         {
             InitializeComponent();
